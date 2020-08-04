9830418d4f353a83201db2edd56d8805627f69ca
stevleibelt <stev@leibelt.de>
2020-08-04 16:45:17 +0200

    refactored CommandBuilderFactory

--- source/CommandBuilderFactory.php
+++ source/CommandBuilderFactory.php
@@ -6,10 +6,12 @@
 namespace De\Leibelt\SendMail;
 
 use De\Leibelt\SendMail\Service\SwiftShipper;
+use InvalidArgumentException;
 use RuntimeException;
 use Swift_Mailer;
 use Swift_SendmailTransport;
 use Swift_SmtpTransport;
+use Swift_Transport;
 
 class CommandBuilderFactory
 {
@@ -23,47 +25,36 @@ class CommandBuilderFactory
         );
     }
 
+    private function buildTransport(array $configuration): Swift_Transport
+    {
+        switch ($configuration['active_transporter_class_name']) {
+            case Swift_SendmailTransport::class:
+                $transport  = new Swift_SendmailTransport(
+                    $configuration[Swift_SendmailTransport::class]['command']
+                );
+                break;
+            case Swift_SmtpTransport::class:
+                $transport  = new Swift_SmtpTransport(
+                    $configuration['list_of_transporter_to_arguments'][Swift_SmtpTransport::class]['hostname'],
+                    $configuration['list_of_transporter_to_arguments'][Swift_SmtpTransport::class]['port']
+                );
+                break;
+            default:
+                throw new InvalidArgumentException(
+                    sprintf(
+                        '[%s]: unsupported swift transporter.', __METHOD__
+                    )
+                );
+        }
+
+        return $transport;
+    }
+
     private function buildShipper(): SwiftShipper
     {
         if (class_exists('Swift_Mailer')) {
-            $configurationPath  = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'configuration';
-
-            $localConfigurationFilePath = $configurationPath . DIRECTORY_SEPARATOR . 'local.php';
-            $distConfigurationFilePath  = $configurationPath . DIRECTORY_SEPARATOR . 'local.dist.php';
-
-            if (file_exists($localConfigurationFilePath)) {
-                $configuration = require_once $localConfigurationFilePath;
-            } else if (file_exists($distConfigurationFilePath)) {
-                $configuration = require_once $distConfigurationFilePath;
-            } else {
-                throw new \RuntimeException(
-                    sprintf(
-                        '[%s] Neither local nor distribution configuration found in path >>%s<<.',
-                        __METHOD__,
-                        $configurationPath
-                    )
-                );
-            }
-
-            switch ($configuration['transporter']['active_transporter_class_name']) {
-                case Swift_SendmailTransport::class:
-                    $transport  = new Swift_SendmailTransport(
-                        $configuration['transporter'][Swift_SendmailTransport::class]['command']
-                    );
-                    break;
-                case Swift_SmtpTransport::class:
-                    $transport  = new Swift_SmtpTransport(
-                        $configuration['transporter']['list_of_transporter_to_arguments'][Swift_SmtpTransport::class]['hostname'],
-                        $configuration['transporter']['list_of_transporter_to_arguments'][Swift_SmtpTransport::class]['port']
-                    );
-                    break;
-                default:
-                    throw new \InvalidArgumentException(
-                        sprintf(
-                            '[%s]: unsupported swift transporter.', __METHOD__
-                        )
-                    );
-            }
+            $configuration = $this->loadConfiguration();
+            $transport = $this->buildTransport($configuration['transporter']);
 
             return new SwiftShipper(
                 new Swift_Mailer(
@@ -75,5 +66,29 @@ class CommandBuilderFactory
                 'could not create a shipper, no need and supported library installed'
             );
         }
+    }
+
+    private function loadConfiguration(): array
+    {
+        $configurationPath  = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'configuration';
+
+        $localConfigurationFilePath = $configurationPath . DIRECTORY_SEPARATOR . 'local.php';
+        $distConfigurationFilePath  = $configurationPath . DIRECTORY_SEPARATOR . 'local.dist.php';
+
+        if (file_exists($localConfigurationFilePath)) {
+            $configuration = require_once $localConfigurationFilePath;
+        } else if (file_exists($distConfigurationFilePath)) {
+            $configuration = require_once $distConfigurationFilePath;
+        } else {
+            throw new RuntimeException(
+                sprintf(
+                    '[%s] Neither local nor distribution configuration found in path >>%s<<.',
+                    __METHOD__,
+                    $configurationPath
+                )
+            );
+        }
+
+        return $configuration;
     }
 }
